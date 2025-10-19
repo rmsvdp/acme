e6f220faed8d982cb35f7d424083acde75e2b8bb
rmsvdp <rmsvdp2022@gmail.com>
2025-10-19 21:39:38 +0200

    usuario.phop documentada

--- Usuario.php
+++ Usuario.php
@@ -1,10 +1,27 @@
 <?php
+
+/**
+ * @category class
+ * Usuario :clase que modela un usuario de aplicación
+ * 
+ */
+
 class Usuario{
 
+    /** @var  String $nombre Nombre del usuario*/
     public $nombre;
+    /** @var  String $pwd Contraseña del usuario*/
     public $pwd;
+    /** @var  String $rol Nombre del usuario*/
     public $rol;
 
+    /**
+     * Constructor de la clase
+     *
+     * @param String $n valor para el nombre
+     * @param String $p valor para la contraseña
+     * @param String $r valor para el rol
+     */
     public function __construct($n,$p,$r){
 
         $this->nombre = $n;
